c52154bd0bc57f625af547b08ed56cdb3db918e2
Akhila <dakhilamallesh2001@gmail.com>
2024-08-14 18:46:15 +0530

    Add project files.

---
+++ Assignment-7/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Assignment_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution sol = new Solution();
+            
+            int[] A1 = { 5, 3, -6, -4, 10, 2 };
+            Console.WriteLine(sol.solution(A1)); 
+
+            int[] A2 = { 1, 2, 3 };
+            Console.WriteLine(sol.solution(A2)); 
+
+            int[] A3 = { -1, -3 };
+            Console.WriteLine(sol.solution(A3)); 
+        }
+    }
+}
---
+++ Assignment-7/Solution.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_7
+{
+    internal class Solution
+    {
+        public int solution(int[] A)
+        {
+            HashSet<int> positiveNumbers = new HashSet<int>();
+
+            // Add all positive numbers to the HashSet
+            foreach (int number in A)
+            {
+                if (number > 0)
+                {
+                    positiveNumbers.Add(number);
+                }
+            }
+
+            // Find the smallest positive integer that occurs in the array
+            int smallestPositiveInteger = 0;
+            for (int i = 1; i <= 1000000; i++)
+            {
+                if (positiveNumbers.Contains(i))
+                {
+                    smallestPositiveInteger = i;
+                    break;
+                }
+            }
+
+            // Return the square of the smallest positive integer
+            return smallestPositiveInteger * smallestPositiveInteger;
+        }
+    }
+}
